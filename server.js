b6ef11107cd7704d94ecdfc29d16f54fc879930d
Harshad <mahajanharsh932@gmail.com>
2024-09-24 21:21:09 +0530

    setup

---
+++ server.js
@@ -0,0 +1,17 @@
+import express from "express";
+import dotenv from "dotenv";
+
+const app=express();
+dotenv.config();
+
+app.get("/",(req,res)=>{
+
+    res.send("Hii from GOVRISI");
+
+});
+
+app.listen(process.env.PORT,()=>{
+
+    console.log("Server running:"+process.env.PORT)
+
+});
