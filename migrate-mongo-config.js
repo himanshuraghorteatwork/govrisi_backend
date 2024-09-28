13de6cac378689e7f8e1d7f1ced19c3f91d63433
Harshad <mahajanharsh932@gmail.com>
2024-09-28 16:22:47 +0530

    migration

---
+++ migrate-mongo-config.js
@@ -0,0 +1,25 @@
+// Import dotenv to access environment variables
+import 'dotenv/config';
+
+export default {
+  mongodb: {
+    // Use the environment variable for the MongoDB connection string
+    url: process.env.MONGO_URI,
+    
+    databaseName: 'gtest',  // Name of your MongoDB database
+    
+    options: {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    },
+  },
+
+  // Directory where migrations are stored
+  migrationsDir: "migrations",
+
+  // MongoDB collection where the changelog will be stored
+  changelogCollectionName: "changelog",
+
+  // Optional: MongoDB connection settings
+  migrationFileExtension: ".js", // Use ".js" for JavaScript migrations
+};
---
+++ migrations/test.js
@@ -0,0 +1,10 @@
+// Add field to the "users" collection
+export async function up(db, client) {
+   
+  }
+  
+  // Remove field from the "users" collection
+  export async function down(db, client) {
+    
+  }
+  
